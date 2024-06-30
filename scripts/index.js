783f006e4772dbfabbc003b5f4f9663c18503464
Aleksey Makshakov <ras11.ru@mail.ru>
2024-06-30 19:01:27 +0500

    FinalVer 1.0

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,26 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const container = document.querySelector(".content");
+const placesList = container.querySelector(".places__list");
+const cardTemplate = document.querySelector("#card-template").content;
+const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+const cardDeleteButton = cardElement.querySelector(".card__delete-button");
+
+const createCard = initialCards.forEach(function (item) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardDeleteButton = cardElement.querySelector(".card__delete-button");
+
+  cardElement.querySelector(".card__image").src = item.link;
+  cardElement.querySelector(".card__title").textContent = item.name;
+
+  cardDeleteButton.addEventListener("click", cardDelete);
+
+  placesList.append(cardElement);
+});
+
+function cardDelete(event) {
+  event.target.closest(".card").remove();
+}
