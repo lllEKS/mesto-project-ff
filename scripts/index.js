0b6abfcccbf2d91ebb2cd97f76ee43e5219ee4f6
Aleksey Makshakov <ras11.ru@mail.ru>
2024-07-01 11:55:45 +0500

    FinalVer2.0

--- scripts/index.js
+++ scripts/index.js
@@ -11,20 +11,25 @@
 const container = document.querySelector(".content");
 const placesList = container.querySelector(".places__list");
 const cardTemplate = document.querySelector("#card-template").content;
-const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
-const cardDeleteButton = cardElement.querySelector(".card__delete-button");
 
-const createCard = initialCards.forEach(function (item) {
-  const cardTemplate = document.querySelector("#card-template").content;
+function createCard(item) {
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
   const cardDeleteButton = cardElement.querySelector(".card__delete-button");
 
-  cardElement.querySelector(".card__image").src = item.link;
-  cardElement.querySelector(".card__title").textContent = item.name;
+  const cardImage = cardElement.querySelector(".card__image");
+  cardImage.src = item.link;
+  cardImage.alt = item.name;
+
+  const cardTitle = cardElement.querySelector(".card__title");
+  cardTitle.textContent = item.name;
 
   cardDeleteButton.addEventListener("click", cardDelete);
 
-  placesList.append(cardElement);
+  return cardElement;
+}
+
+initialCards.forEach(function (item) {
+  placesList.append(createCard(item));
 });
 
 function cardDelete(event) {
